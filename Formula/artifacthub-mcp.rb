6b3735448d74bdf798e9f18b905c219b01afe1b7
luxass-homebrew <luxass-homebrew[bot]@users.noreply.github.com>
2026-05-30 11:22:45 +0000

    chore: update artifacthub-mcp to 0.2.7

--- Formula/artifacthub-mcp.rb
+++ Formula/artifacthub-mcp.rb
@@ -1,26 +1,26 @@
 class ArtifacthubMcp < Formula
   desc "MCP server for Artifact Hub"
   homepage "https://github.com/luxass/artifacthub-rs"
-  version "0.2.6"
+  version "0.2.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/artifacthub-mcp%40#{version}/artifacthub-mcp-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "4c9a0c607c5d810e6aa02bcea06d402bbd65f10fa5d9bc203a2c6e84680b8cf3" # sha-update-id: artifacthub-mcp-aarch64-apple-darwin
+      sha256 "e45221c37a4bff13131c13e9d282ca0a307e11094b7576b3759b34af645e16cd" # sha-update-id: artifacthub-mcp-aarch64-apple-darwin
     else
       url "#{homepage}/releases/download/artifacthub-mcp%40#{version}/artifacthub-mcp-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ee4d19cdee03357cbf8312726eb5ee4dd3abbcd9cca33bcdf29272a4f1518c84" # sha-update-id: artifacthub-mcp-x86_64-apple-darwin
+      sha256 "92e9d821e522c40b7f119a2f68cf7898d9b8b5ed600e96eea0f6f5b60cd5b439" # sha-update-id: artifacthub-mcp-x86_64-apple-darwin
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/artifacthub-mcp%40#{version}/artifacthub-mcp-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2686b60660fdf54ecbe67dd51bceee77a23823d45174b63565a64d4e08a0ed5d" # sha-update-id: artifacthub-mcp-aarch64-unknown-linux-gnu
+      sha256 "da7c456537eda86518f6789280c5dbc302219f73c8460d1bc99f577a2c2fb30d" # sha-update-id: artifacthub-mcp-aarch64-unknown-linux-gnu
     else
       url "#{homepage}/releases/download/artifacthub-mcp%40#{version}/artifacthub-mcp-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e99d23b24f9c0f39e13e10383ec2b4aee96ea264487d5d5734cbf53fc48cd073" # sha-update-id: artifacthub-mcp-x86_64-unknown-linux-gnu
+      sha256 "0b477777fac90b9f6b6798c127f1ca1681c6ae6e1c0fb0786540da90d7edf172" # sha-update-id: artifacthub-mcp-x86_64-unknown-linux-gnu
     end
   end
 
