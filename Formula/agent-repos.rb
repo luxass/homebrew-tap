c9aef25fd6160b4893e6dd47c972efcd1b85d5c7
Lucas Nørgård <lucasnrgaard@gmail.com>
2026-08-03 04:25:07 +0200

    Update agent-repos.rb

--- Formula/agent-repos.rb
+++ Formula/agent-repos.rb
@@ -7,20 +7,20 @@ class AgentRepos < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/agent-repos-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # sha-update-id: agent-repos-aarch64-apple-darwin
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # sha-update-id: agent-repos-aarch64-apple-darwin
     else
       url "#{homepage}/releases/download/v#{version}/agent-repos-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # sha-update-id: agent-repos-x86_64-apple-darwin
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # sha-update-id: agent-repos-x86_64-apple-darwin
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/agent-repos-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # sha-update-id: agent-repos-aarch64-unknown-linux-gnu
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # sha-update-id: agent-repos-aarch64-unknown-linux-gnu
     else
       url "#{homepage}/releases/download/v#{version}/agent-repos-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # sha-update-id: agent-repos-x86_64-unknown-linux-gnu
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # sha-update-id: agent-repos-x86_64-unknown-linux-gnu
     end
   end
 
