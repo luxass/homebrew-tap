36ca0681391062b7349bc799986d066dd1b23ac5
luxass-homebrew <luxass-homebrew[bot]@users.noreply.github.com>
2026-09-19 16:37:33 +0000

    chore: update imessage-relay-server to 0.2.0

--- Formula/imessage-relay-server.rb
+++ Formula/imessage-relay-server.rb
@@ -1,11 +1,11 @@
 class ImessageRelayServer < Formula
   desc "Local HTTP API for Apple Messages"
   homepage "https://github.com/luxass/imessage-relay"
-  version "0.1.2"
+  version "0.2.0"
   license "MIT"
 
   url "#{homepage}/releases/download/v#{version}/imessage-relay-server-#{version}-macos-universal.tar.gz"
-  sha256 "94e1580a8e21bdcddcc0df9801209de4da747a897df6a96f57c2b98a7789471f" # sha-update-id: imessage-relay-server-macos-universal
+  sha256 "d6a5bc552d75fb1b4d3f9d2b98b9a274897235c23d65c9bd0b106f549ca71a5b" # sha-update-id: imessage-relay-server-macos-universal
 
   depends_on :macos
 
