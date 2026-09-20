7570b268fc28f5807b8c3d57407aefd8e7c2e5b7
luxass-homebrew <luxass-homebrew[bot]@users.noreply.github.com>
2026-09-20 14:35:59 +0000

    chore: update imessage-relay-server to 0.3.0

--- Casks/imessage-relay.rb
+++ Casks/imessage-relay.rb
@@ -1,6 +1,6 @@
 cask "imessage-relay" do
   version "0.3.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # sha-update-id: imessage-relay-macos-universal
+  sha256 "ad2d2fcbff7dcd3d5a49676303e238f204c7e250d1a7d4c68497835ef363757e" # sha-update-id: imessage-relay-macos-universal
 
   url "https://github.com/luxass/imessage-relay/releases/download/v#{version}/imessage-relay-#{version}-macos-universal.zip"
   name "iMessage Relay"
--- Formula/imessage-relay-server.rb
+++ Formula/imessage-relay-server.rb
@@ -1,11 +1,11 @@
 class ImessageRelayServer < Formula
   desc "Local HTTP API for Apple Messages"
   homepage "https://github.com/luxass/imessage-relay"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   url "#{homepage}/releases/download/v#{version}/imessage-relay-server-#{version}-macos-universal.tar.gz"
-  sha256 "d6a5bc552d75fb1b4d3f9d2b98b9a274897235c23d65c9bd0b106f549ca71a5b" # sha-update-id: imessage-relay-server-macos-universal
+  sha256 "6367b1db054c2156f7e2752c264980ffb36c44d11824706dc5e26dab6cde3375" # sha-update-id: imessage-relay-server-macos-universal
 
   depends_on :macos
 
